eebfdb41dabc4fff170403f9252b1c01558069f2
Manuelortegacatalan <m.nacho91@gmail.com>
2016-05-18 17:04:00 -0400

    proyecto arduino

---
+++ src/interruptorUno/ArduinoSwitch.java
@@ -0,0 +1,13 @@
+
+package interruptorUno;
+
+
+public class ArduinoSwitch {
+
+    
+    public static void main(String[] args) {
+    Window Ventana = new Window();
+    Ventana.setVisible(true);
+
+    }
+}
